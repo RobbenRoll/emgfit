1b28356e948bb149bade6c597f088cd32369cb6c
Stefan Paul <RobbenRoll@users.noreply.github.com>
2020-08-07 23:58:41 -0700

    Add tunable truncation range to h_m_emg & h_p_emg calculations to enable balancing of speed and accuracy.

--- emgfit/emg_funcs.py
+++ emgfit/emg_funcs.py
@@ -19,16 +19,11 @@ exp_mp = np.frompyfunc(mp.exp,1,1)
 ################################################################################
 ##### Define general Hyper-EMG functions
 
-norm_precision = 1e-06 # level on which eta parameters must agree with unity 
+norm_precision = 1e-06 # level on which eta parameters must agree with unity
 
-# def bounded_exp(arg):
-#     """ Numerically stable exponential function which avoids under- or overflow by setting bounds on argument
-#     """
-#     max_arg = 680 # max_arg = 600 results in maximal y-value of 3.7730203e+260
-#     min_arg = -1000000000000000000
-#     arg = np.where(arg > max_arg, max_arg, arg)
-#     arg = np.where(arg < min_arg, min_arg, arg)
-#     return np.exp(arg)
+from warnings import warn
+trunc_range = 40*2.355 # truncation range in units of sigma
+trunc_ymax = 1e-05 # maximal y-value to be truncated, raises warning if normalized y-values are lower at edges of truncation range
 
 @njit #(parallel=True)
 def h_m_i(x,mu,sigma,eta_m,tau_m):
@@ -62,8 +57,6 @@ def h_m_emg(x, mu, sigma, li_eta_m,li_tau_m):
         Tuple containing the neg. tail decay constants with the signature:
         ``(tau_m1, tau_m2, ...)``.
 
-
-
     Returns
     -------
     float
@@ -86,18 +79,21 @@ def h_m_emg(x, mu, sigma, li_eta_m,li_tau_m):
     if len(li_tau_m) != t_order_m:  # check if all arguments match tail order
         raise Exception("orders of eta_m and tau_m do not match!")
 
+    # Create mask
+    mask = (np.abs(x - mu) < 0.5*trunc_range*sigma)
     h_m = 0.
     for i in range(t_order_m):
         eta_m = li_eta_m[i]
         tau_m = li_tau_m[i]
-        h_i = h_m_i(x,mu,sigma,eta_m,tau_m)
-        if np.all(np.isfinite(h_i)):
-            h_m += h_i #nan_to_num(h_i) #where(isfinite(h_i),h_i,0.)
-        else:
-            h_m += h_m_i_prec(x,mu,sigma,eta_m,tau_m)
-            print("h_m_i_prec")
-        #if not np.all(np.isfinite(h_i)):
-        #    print("m",i,h_i)
+        h_i = np.zeros_like(x)
+        h_i[mask] = h_m_i(x[mask],mu,sigma,eta_m,tau_m)
+        if not np.all(np.isfinite(h_i)):
+            h_i[mask] = h_m_i_prec(x[mask],mu,sigma,eta_m,tau_m)
+        if len(mask) < len(x) and np.any(h_i[mask][[0,-1]] > trunc_ymax):
+            msg = str("WARNING: h_m_emg values at edges of truncation range are > {0}. "
+                  "Consider increasing the mask range.").format(trunc_ymax)
+            warn(msg)
+        h_m += h_i
     return h_m
 
 
@@ -156,16 +152,21 @@ def h_p_emg(x, mu, sigma, li_eta_p, li_tau_p):
     if len(li_tau_p) != t_order_p:  # check if all arguments match tail order
         raise Exception("orders of eta_p and tau_p do not match!")
 
+   # Create mask
+    mask = (np.abs(x - mu) < 0.5*trunc_range*sigma)
     h_p = 0.
     for i in range(t_order_p):
         eta_p = li_eta_p[i]
         tau_p = li_tau_p[i]
-        h_i = h_p_i(x,mu,sigma,eta_p,tau_p)
-        if np.all(np.isfinite(h_i)):
-            h_p += h_i #nan_to_num(h_i) #where(isfinite(h_i),h_i,0.)
-        else:
-            h_p += h_p_i_prec(x,mu,sigma,eta_p,tau_p)
-            print("h_p_i_prec")
+        h_i = np.zeros_like(x)
+        h_i[mask] = h_p_i(x[mask],mu,sigma,eta_p,tau_p)
+        if not np.all(np.isfinite(h_i)):
+            h_i[mask] = h_p_i_prec(x[mask],mu,sigma,eta_p,tau_p)
+        if len(mask) < len(x) and np.any(h_i[mask][[0,-1]] > trunc_ymax):
+            msg = str("WARNING: h_p_emg values at edges of truncation range are > {0}. "
+                  "Consider increasing the mask range.").format(trunc_ymax)
+            warn(msg)
+        h_p += h_i
     return h_p
 
 
