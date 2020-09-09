5a53c0fa6d6ce0948987b16acfdf64371454e97b
RobbenRoll <stefan.paul@online.de>
2020-09-09 16:01:46 -0700

    Add GITHUB_TOKEN and implement it in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-# Script based off of https://github.com/voorhoede/front-end-tooling-recipes/blob/master/travis-deploy-to-gh-pages/scripts/deploy.sh
 #!/bin/bash
+# Script based off of https://github.com/voorhoede/front-end-tooling-recipes/blob/master/travis-deploy-to-gh-pages/scripts/deploy.sh
 
 set -e # exit with nonzero exit code if anything fails
 
@@ -11,7 +11,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
 #using token clone gh-pages branch
-git clone --branch=gh-pages https://${GH_USER}:${DOCTR_DEPLOY_ENCRYPTION_KEY_ROBBENROLL_EMGFIT}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GH_USER}:${GITHUB_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
 
 # Update gh-pages index
 cd gh-pages
@@ -20,6 +20,6 @@ bash ./make-index.sh
 #add, commit and push files
 git add -f .
 git commit --allow-empty -m "Update gh-pages index with travis build $TRAVIS_BUILD_NUMBER"
-git push -f origin gh-pages
+git push -fq origin gh-pages
 
 echo "Done updating gh-pages index"
