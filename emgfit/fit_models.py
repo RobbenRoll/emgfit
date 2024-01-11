FIX peakshape error evaluation failing due to parameters outside bounds or precision loss in mu0 calculation.

emgfit/fit_models.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import lmfit as fit
 import sys
+import warnings
 from .config import *
 from .emg_funcs import *
 from scipy.special import erfcx
@@ -218,7 +219,10 @@ def erfcxinv(y):
     # to erfcx
     ret[~mask] = -sqrt(np.log(y[~mask]))
     for n in range(7):
-        ret += - (erfcx(ret) - y)/(2*ret*erfcx(ret) - 2/sqrt(pi))
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=RuntimeWarning)
+            denom = 2*ret*erfcx(ret) - 2/sqrt(pi)
+            ret += np.where(denom==0, 0.0, - (erfcx(ret) - y)/denom)
     return ret
 
 
@@ -283,7 +287,7 @@ def get_mu0(x_m, init_pars, fit_model):
         if t_order_p == 1:
             tau_p1 = init_pars['tau_p1']
             sum_p = (sqrt(2)*sigma*erfcxinv(tau_p1/sigma*sqrt(2/pi))
-                        - sigma**2/tau_p1)
+                     - sigma**2/tau_p1)
         else:
             for i in range(t_order_p):
                 eta_pi = init_pars['eta_p{}'.format(i+1)]
@@ -300,10 +304,9 @@ def get_mu0(x_m, init_pars, fit_model):
 
         mu0 = x_m - theta*sum_m + (1-theta)*sum_p
     else:
-        msg = str("fit_model must 'Gaussian' or 'emgXY' w/ X & Y being the "
-                  "order of neg. & pos. exponential tails respectively!")
+        msg = str("fit_model must be 'Gaussian' or 'emgXY' with X & Y denoting the tail orders!")
         raise Exception(msg)
-
+    
     return mu0
 
 

emgfit/spectrum.py
@@ -3671,6 +3671,9 @@ class spectrum:
         for par in shape_pars:
             pars = copy.deepcopy(self.shape_cal_pars) # avoid changes in original dict
             pars[par] = self.shape_cal_pars[par] + self.shape_cal_errors[par]
+            scal_pref = "p{}_".format(self.index_shape_calib)
+            if pars[par] > self.shape_cal_result.params[scal_pref+par].max:
+                pars[par] = self.shape_cal_result.params[scal_pref+par].max
             if par == 'delta_m':
                 pars['eta_m2'] = pars[par] - self.shape_cal_pars['eta_m1']
                 pars['eta_m3'] = 1 - self.shape_cal_pars['eta_m1'] - pars['eta_m2']
@@ -3697,6 +3700,8 @@ class spectrum:
             #display(fit_result_p) # show fit result
 
             pars[par] = self.shape_cal_pars[par] - self.shape_cal_errors[par]
+            if pars[par] < self.shape_cal_result.params[scal_pref+par].min:
+                pars[par] = self.shape_cal_result.params[scal_pref+par].min
             if par == 'delta_m':
                 pars['eta_m2'] =  pars[par] - self.shape_cal_pars['eta_m1']
                 pars['eta_m3'] = 1 - self.shape_cal_pars['eta_m1'] - pars['eta_m2']
@@ -3835,7 +3840,7 @@ class spectrum:
             p.rel_peakshape_error = PS_mass_error/m_ion
             if p._stat_area_error is None:
                 p._stat_area_error = self.calc_peak_area(peak_idx,
-                                                       fit_result=fit_result)[1]
+                                                         fit_result=fit_result)[1]
             p.area_error = np.sqrt(p._stat_area_error**2 + PS_area_error**2)
             # remove MC PS error flag (if applicable)
             if peak_idx in self.peaks_with_MC_PS_errors:
@@ -4393,7 +4398,7 @@ class spectrum:
                     msg = str("Properties of peak {} not updated since "
                               "MC estimates of mass or area peak-shape error "
                               "are NaN.").format(peak_idx)
-                    warings.warn(msg)
+                    warnings.warn(msg)
                     continue # skip updating properties of this peak
                 p = self.peaks[peak_idx]
                 pref = 'p{0}_'.format(peak_idx)